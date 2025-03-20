ddb776445ec0915b3c873978817abea670c3fea6
Mario Papace <papace@aicomp04.math.uni-wuppertal.de>
2025-03-20 13:11:05 +0100

    in probing.c added a command to decide which grids have to be colored

--- src/probing.c
+++ src/probing.c
@@ -19,17 +19,6 @@ void vector_copy(int *dest, int *src, int size) {
     }
 }
 
-/*
-void colors_init(){
-    MALLOC(g.num_colors, int, g.num_levels);
-    
-    g.colors = (int**)malloc(g.num_levels * sizeof(int*));
-    
-    if (g.colors == NULL)
-        error0("Allocation error0\n");
-}
-*/
-
 void setup_local_colors(){
     
     int num_processes;
@@ -218,6 +207,8 @@ void graph_coloring() {
     int total_points = T * Z * Y * X;
     
     MALLOC(g.colors[level], int, total_points);
+    
+    if(level<g.coloring_method){
 
     // Set all colors to 0 (not assigned)
     for (int i = 0; i < total_points; i++) {
@@ -262,6 +253,12 @@ void graph_coloring() {
             }
         }
     }
+    }
+    else{
+        for(int i = 0; i < total_points; i++)
+            g.colors[level][i] = 1;
+        }
+        
     g.num_colors[level] = max(g.colors[level], total_points);
     
     }
@@ -279,132 +276,3 @@ void graph_coloring() {
 }
 
 
-// DOWN FOR A MIXTURE OF THE COLORING METHODS 
-// UP FOR JUST ONE OF THE TWO ---> generate_neighbors() function is needed for both, do not comment it!
-
-/*
-void greedy_coloring(int *colors, int total_points, int size[4]) {
-
-    // Set all colors to 0 (not assigned)
-    for (int i = 0; i < total_points; i++) {
-        colors[i] = 0;
-    }
-
-    // Iterate over all lattice sites
-    for (int t = 0; t < size[0]; t++) {
-        for (int z = 0; z < size[1]; z++) {
-            for (int y = 0; y < size[2]; y++) {
-                for (int x = 0; x < size[3]; x++) {
-                    int index = lex_index(t, z, y, x, size);
-
-                    // Skip if the site has already been assigned a color
-                    if (colors[index] != 0) {
-                        continue;
-                    }
-
-                    // Find neighbors within distance d
-                    int *neighbors;
-                    int num_neighbors;
-                    generate_neighbors(t, z, y, x, &neighbors, &num_neighbors, size);
-
-                    int used_colors[256] = {0}; 
-                    for (int i = 0; i < num_neighbors; i++) {
-                        int neighbor_index = neighbors[i];
-                        if (colors[neighbor_index] != 0) {
-                            used_colors[colors[neighbor_index] - 1] = 1;
-                        }
-                    }
-
-                    // Assign the minimum possible color
-                    int color = 1;
-                    while (used_colors[color - 1]) {
-                        color++;
-                    }
-                    colors[index] = color;
-
-                    // Free memory of neighbors
-                    free(neighbors);
-                }
-            }
-        }
-    }
-    
-    }
-
-void coloring_scheme(int *colors, int size[4]){
-    
-    int coordinates[4];
-    
-    for(int t = 0; t < size[0]; t++){
-        coordinates[0] = t;
-        for(int z = 0; z < size[1]; z++){
-            coordinates[1] = z;
-            for(int y = 0; y < size[2]; y++){
-                coordinates[2] = y;
-                for(int x = 0; x < size[3]; x++){
-                    coordinates[3] = x;
-                    int idx = lex_index(t, z, y, x, size);
-
-                    int col = 0;
-                    int power = 1;
-                    for(int k = 0; k<4; k++){
-                        int w_tilde = coordinates[k]%(g.coloring_distance + 1);
-                        col += w_tilde * power;
-                        power *= (g.coloring_distance + 1);
-	               }    
-	               colors[idx] = col + 1;
-               }
-            }
-        }
-    }
-    
-}
-
-
-
-void graph_coloring(){
-    
-    struct timespec start, end;
-    double time_taken;
-    
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    
-    colors_init();
-    
-    for(int level = 0; level < g.num_levels; level++){
-        
-        int T = g.global_lattice[level][0];
-        int Z = g.global_lattice[level][1];
-        int Y = g.global_lattice[level][2];
-        int X = g.global_lattice[level][3];
-    
-        int size[4];
-    
-        size[0] = T;
-        size[1] = Z;
-        size[2] = Y;
-        size[3] = X;
-
-        int total_points = T * Z * Y * X;
-    
-        MALLOC(g.colors[level], int, total_points);
-        
-        if(level < g.coloring_method)
-            coloring_scheme(g.colors[level], size);
-        else
-            greedy_coloring(g.colors[level], total_points, size);
-        
-        g.num_colors[level] = max(g.colors[level], total_points);
-        
-    }
-    
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    
-    time_taken = (end.tv_sec - start.tv_sec) +
-                 (end.tv_nsec - start.tv_nsec) / 1e9;
-                 
-    if(g.my_rank == 0)
-        printf("\nTime for coloring: %f seconds\n", time_taken);
-    
-}
-*/
